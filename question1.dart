e4b9af61a6ae0442b87476a2b6b36ee2cd7ff8c1
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2023-07-06 01:13:49 +0500

    added question 1

---
+++ question1.dart
@@ -0,0 +1,12 @@
+void printEvenNumbers(List<int> numbers) {
+  for (int num in numbers) {
+    if (num % 2 == 0) {
+      print(num);
+    }
+  }
+}
+
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  printEvenNumbers(numbers);
+}
